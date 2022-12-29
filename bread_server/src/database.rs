6081c4f12fb297fc792cddc4dc6e569ba9ebffdd
Aurelia Augusta <aurelia@aeva.dev>
2022-12-29 05:06:47 -0500

    Refactored some of the code to have tests and also fixup some of the hierarchy

--- bread_server/src/database.rs
+++ bread_server/src/database.rs
@@ -2,3 +2,6 @@
 /// For the purposes of this application, SQLite was chosen as the data storage engine due to the low overhead and reliability
 pub mod models;
 pub mod startup;
+
+// here's all our testing modules!
+mod startup_test;
