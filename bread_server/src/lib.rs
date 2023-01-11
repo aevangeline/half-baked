86c2597fad2fd1eec53eb5d05e0987fab493ef9a
Aurelia Augusta <aurelia@aeva.dev>
2023-01-10 21:15:16 -0500

    Ran Cargo fmt

--- bread_server/src/lib.rs
+++ bread_server/src/lib.rs
@@ -1,15 +1,23 @@
-/**
- * lib.rs - this contains the top-level logic of half-baked's server, and handles setup, shutdown, and all the other admin tasks
- */
+/// lib.rs - this contains the top-level logic of half-baked's server, and handles setup, shutdown, and all the other admin tasks
 pub mod database;
 use directories::BaseDirs;
-use std::io;
+use env_logger;
+use log;
+use log::LevelFilter;
+use std::io::{Error, ErrorKind};
 
 // connects to the database
 pub fn connect_to_db() -> Result<(), io::Error> {
     if let Some(base_dirs) = BaseDirs::new() {
         let db_path = base_dirs.data_dir().join("half-baked/development.db");
         database::startup::initialize_db(db_path);
+        log
     }
-    Ok(())
+    Err(Error::from(ErrorKind::NotFound))
+}
+
+/// setups up the logger for the package
+pub fn setup_logger() {
+    let mut builder = env_logger::Builder::new().filter_level(LevelFilter::Warn);
+    builder.init();
 }
