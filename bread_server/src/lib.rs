b4e428b16b45ca6e88afe29301208aeed5dea6d7
Aurelia Augusta <aurelia@aeva.dev>
2023-01-19 20:28:23 -0500

    Fixing some compile bugs

--- bread_server/src/lib.rs
+++ bread_server/src/lib.rs
@@ -4,17 +4,20 @@ use directories::BaseDirs;
 use env_logger;
 use log;
 use log::LevelFilter;
+use std::io;
 use std::io::{Error, ErrorKind};
 
 // connects to the database
 pub fn connect_to_db() -> Result<(), io::Error> {
     if let Some(base_dirs) = BaseDirs::new() {
-        let db_path = base_dirs.data_dir().join("half-baked/development.db");
-        database::startup::initialize_db(db_path);
+        let db_path_buf = base_dirs.data_dir().join("half-baked/development.db");
+        let db_path = db_path_buf.as_path();
+        database::startup::initialize_db(db_path)?;
         log::info!(
             "Connected to DB @ {}",
             db_path.to_str().unwrap_or("No Path Found")
         );
+        return Ok(());
     }
     log::error!("No available system-defined data directory, cannot open database");
     Err(Error::from(ErrorKind::NotFound))
@@ -22,7 +25,8 @@ pub fn connect_to_db() -> Result<(), io::Error> {
 
 /// setups up the logger for the package
 pub fn setup_logger() {
-    let mut builder = env_logger::Builder::new().filter_level(LevelFilter::Warn);
+    let mut builder = env_logger::Builder::new();
+    builder.filter_level(LevelFilter::Warn);
     log::info!("Initialized Logging");
     builder.init();
 }
