ce296e7c90a7c45673dbbcb931f7ea5ade27fe2c
Aurelia Augusta <aurelia@aeva.dev>
2023-01-20 09:52:44 -0500

    Fixing a simple build issue with protobufs

--- bread_server/src/build.rs
+++ bread_server/src/build.rs
@@ -1,9 +1,6 @@
 fn main() -> Result<()> {
     prost_build::compile_protos(
-        &[
-            "../shared_resources/protobufs/users.proto",
-            "../shared_resources/protobufs/backend.proto",
-        ],
+        &["../shared_resources/protobufs/rpc.proto"],
         &["../shared_resources/protobufs"],
     )?;
     Ok(())
