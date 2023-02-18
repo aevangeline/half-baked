8c9df904820caf3b2bd2a87b3d067bfc1f0d46f1
Aurelia Augusta <aurelia@aeva.dev>
2023-02-18 14:53:11 -0500

    Starting work on the rpc server implmentation

--- bread_server/src/database/startup.rs
+++ bread_server/src/database/startup.rs
@@ -5,6 +5,7 @@ use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
 use std::fs;
 use std::io::{Error, ErrorKind};
 use std::path::Path;
+use std::sync::Arc;
 /// MIGRATIONS is a compile-time pack-in of all pending migrations for this application, this will allow you to
 const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
 
@@ -13,7 +14,7 @@ const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
 ///     db_path : AsRef<Path> -> a path-like object which points
 ///            to either an existing sqlite database OR a valid place we can create one
 ///
-pub fn initialize_db<P: AsRef<Path>>(db_path: P) -> Result<(), Error> {
+pub fn initialize_db<P: AsRef<Path>>(db_path: P) -> Result<Arc<SqliteConnection>, Error> {
     // if a file already exists, then we're all ok!
     let p = db_path.as_ref();
 
@@ -29,32 +30,35 @@ pub fn initialize_db<P: AsRef<Path>>(db_path: P) -> Result<(), Error> {
         .map(fs::create_dir_all)?; // Try to create the containing directory for our database, return an error if we fail
 
     // we have to convert the Path to a string to use it with diesel to create a connection
-    let mut conn = p
-        .to_str()
-        .ok_or_else(|| {
-            Error::new(
-                ErrorKind::NotFound,
-                format!(
-                    "cannot create a string version of the path to the db : {:?}",
-                    p
-                ),
-            )
-        })
-        .and_then(|path| {
-            SqliteConnection::establish(path) // We try to create the connection here and fail over into an io:Error if we can't
-                .map_err(|_| {
-                    Error::new(
-                        ErrorKind::InvalidInput,
-                        format!("cannot create or connect to db : {:?}", p),
-                    )
-                })
-        })?;
-    conn.run_pending_migrations(MIGRATIONS).map_err(|_| {
+    let mut conn = Arc::new(
+        p.to_str()
+            .ok_or_else(|| {
+                Error::new(
+                    ErrorKind::NotFound,
+                    format!(
+                        "cannot create a string version of the path to the db : {:?}",
+                        p
+                    ),
+                )
+            })
+            .and_then(|path| {
+                SqliteConnection::establish(path) // We try to create the connection here and fail over into an io:Error if we can't
+                    .map_err(|_| {
+                        Error::new(
+                            ErrorKind::InvalidInput,
+                            format!("cannot create or connect to db : {:?}", p),
+                        )
+                    })
+            })?,
+    );
+    let mut bare_conn = &mut *Arc::get_mut(&mut conn).unwrap();
+
+    bare_conn.run_pending_migrations(MIGRATIONS).map_err(|_| {
         Error::new(
             ErrorKind::InvalidInput,
             format!("migrations failed on db! : {:?}", p),
         )
     })?;
 
-    Ok(())
+    Ok(conn)
 }
--- bread_server/src/service/rpc.rs
+++ bread_server/src/service/rpc.rs
@@ -1,5 +1,6 @@
 // rpc.rs -  Here's our implementation of our various rpc functions
 // inject our generated rpc services here
+use std::sync::Arc;
 pub mod bread_service {
     tonic::include_proto!("bread_service");
 }
@@ -9,3 +10,11 @@ use bread_service::{
     bread_service_server::{BreadService, BreadServiceServer},
     GetUsersRequest, GetUsersResponse,
 };
+
+use diesel::SqliteConnection;
+use tonic::{transport::Server, Request, Response, Status};
+
+//BSImpl - a concrete implementation of the breadservice, requires a db connection to actually work!
+pub struct BSImpl {
+    conn: Arc<SqliteConnection>,
+}
